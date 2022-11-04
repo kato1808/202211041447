4af17859ef3a04f307589f963b94e669a5d04467
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-04 14:50:17 +0900

    202211041450

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -10,7 +10,7 @@ export const Page1 = () => {
 
   return (
     <div>
-      <h1>Page1ページです</h1>
+      <h1>Page1ページです!</h1>
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
