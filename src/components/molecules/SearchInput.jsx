4a8c42845dbefc0fb525d1aa02be8ec0b8ab16c6
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-04 16:25:58 +0900

    202211041625

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/Input";
+
+export const SearchInput = () => {
+  return (
+    <SCotainer>
+      <Input placeholder="検索条件を入力" />
+      <SBouttnWrapper>
+        <PrimaryButton>検索</PrimaryButton>
+      </SBouttnWrapper>
+    </SCotainer>
+  );
+};
+
+const SCotainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SBouttnWrapper = styled.div`
+  padding-left: 8px;
+`;
