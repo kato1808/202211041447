213abe64e73456c04fe768fcd61271b91450a34e
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-08 10:47:59 +0900

    202211081047

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>123456@pasco.com</dd>
+        <dt>電話</dt>
+        <dd>080-1074-7397</dd>
+        <dt>会社名</dt>
+        <dd>株式会社パスコ</dd>
+        <dt>ホームページ</dt>
+        <dd>https:pasco.com</dd>
+      </dl>
+    </div>
+  );
+};
