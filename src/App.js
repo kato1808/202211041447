34fe91707812100e502ddb61358320c0754f7305
kato1808 <111556812+kato1808@users.noreply.github.com>
2022-11-09 10:25:50 +0900

    202211091025

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
 
 const user = {
@@ -19,13 +20,13 @@ const user = {
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <HeaderOnly>
         <PrimaryButton>テスト!!</PrimaryButton>
         <SecondaryButton>検索</SecondaryButton>
         <br />
         <SearchInput />
         <UserCard user={user} />
-      </div>
+      </HeaderOnly>
     </BrowserRouter>
   );
 }
